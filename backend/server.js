b52070c2fa379eb049705bb200bd1d102b4e6c53
RougeByte <sarveshdange1903@gmail.com>
2026-05-24 15:48:15 +0530

    Updated frontend CORS URL

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cors({
     origin: [
         'http://localhost:3000',
-        'https://your-frontend-name.onrender.com'
+        'https://bugx-frontend.onrender.com'
     ],
     credentials: true
 }));
